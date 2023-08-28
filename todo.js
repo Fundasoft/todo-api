ea87c50afbfef56573f7b79d62220e40cb8abbdf
Mariano Piriz <mariano.piriz.acevedo@gmail.com>
2023-08-28 20:34:59 -0300

    actualizacion CORS

--- todo.js
+++ todo.js
@@ -3,19 +3,23 @@ const cors = require('cors');
 const app = express();
 const router = require('./router');
 
-app.use(cors({
-  origin: 'http://localhost:3001',
-}));
+app.use(
+  cors({
+    origin:
+      'http://fundasoft-todo-app-mariano.s3-website-us-east-1.amazonaws.com',
+  })
+);
 app.use(express.json());
 
 app.use('/todos', router);
 
-app.use(function (err, req, res, next) { // middleware de error
+app.use(function (err, req, res, next) {
+  // middleware de error
   console.log(err);
   res.send('Algo salió mal');
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('¡La API está en funcionamiento!');
 });
 
